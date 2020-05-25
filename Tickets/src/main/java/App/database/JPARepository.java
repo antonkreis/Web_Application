f6129ec9b0e39d2f349040eddcda79627720d730
Anton Kreis <61134563+antonkreis@users.noreply.github.com>
2020-05-25 13:00:54 +0300

    Delete JPARepository.java

--- Tickets/src/main/java/App/database/JPARepository.java
+++
@@ -1,9 +0,0 @@
-package App.database;
-
-import App.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface JPARepository extends CrudRepository<User, Integer>{
-//    List<Ticket> findByClientName(String lastName);
-//    Ticket findByID(String id);
-}
